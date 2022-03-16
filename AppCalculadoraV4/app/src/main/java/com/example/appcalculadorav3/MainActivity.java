5c6d0bba4e6dd4084ffa08008820d516a7ef835c
Vinicius Oliveira <vine0@hotmail.com>
2022-03-16 15:56:30 -0300

    App Calculadora V4
    Variação da V3, porém com spinner dinâmico.

---
+++ AppCalculadoraV4/app/src/main/java/com/example/appcalculadorav3/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.appcalculadorav3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spCalcular;
+    EditText edtV1;
+    EditText edtV2;
+    EditText edtResp;
+
+    List<String> operacoes = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtV1 = (EditText) findViewById(R.id.editValor1);
+        edtV2 = (EditText) findViewById(R.id.editValor2);
+        edtResp = (EditText) findViewById(R.id.editResposta) ;
+        spCalcular = (Spinner) findViewById(R.id.spinner);
+
+        // List
+        operacoes.add("Somar");
+        operacoes.add("Subtrair");
+        operacoes.add("Multiplicar");
+        operacoes.add("Dividir");
+
+        ArrayAdapter<String>  arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, operacoes);
+
+        ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spCalcular.setAdapter(spinnerArrayAdapter);
+    }
+    public void calcular(View v){
+        double v1, v2, resp;
+        if(spCalcular.getSelectedItemId()==0){
+            Calculadora calc = new Calculadora(Double.parseDouble(edtV1.getText().toString()),Double.parseDouble(edtV2.getText().toString()));
+            edtResp.setText(calc.formatar(calc.somar(), 2));
+        }
+        else if(spCalcular.getSelectedItemId()==1){
+            Calculadora calc = new Calculadora(Double.parseDouble(edtV1.getText().toString()),Double.parseDouble(edtV2.getText().toString()));
+            edtResp.setText(calc.formatar(calc.subtrair(), 2));
+        }
+        else if(spCalcular.getSelectedItemId()==2){
+            Calculadora calc = new Calculadora(Double.parseDouble(edtV1.getText().toString()),Double.parseDouble(edtV2.getText().toString()));
+            edtResp.setText(calc.formatar(calc.multiplicar(), 2));
+        }
+        else if(spCalcular.getSelectedItemId()==3){
+            Calculadora calc = new Calculadora(Double.parseDouble(edtV1.getText().toString()),Double.parseDouble(edtV2.getText().toString()));
+            edtResp.setText(calc.formatar(calc.dividir(), 2));
+        }
+
+
+
+    }
+}
